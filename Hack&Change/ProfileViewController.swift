e2f1540180b52f449d2fc7988f0ded4a2e45a310
Maxim Polovinkin <polloVinkin@yandex.ru>
2022-11-12 10:18:36 +0300

    add ui

---
+++ Hack&Change/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  Hack&Change
+//
+//  Created by Максим Половинкин on 12.11.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Hack&Change/ViewController.swift
+++ Hack&Change/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var registerButton: UIButton!
+   
+    @IBOutlet weak var enterButton: UIButton!
+   
+    @IBOutlet weak var phoneText: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       //sdfsdf
+        roundCorners()
     }
-
-
+    
+    @IBAction func registerTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func enterTapped(_ sender: Any) {
+        let vc = storyboard?.instantiateViewController(withIdentifier: "ProfileVc")
+        vc?.modalPresentationStyle = .fullScreen
+        vc?.modalTransitionStyle = .crossDissolve
+        self.present(vc!, animated: true)
+    }
+    
+    func roundCorners() {
+        passwordTextField.layer.cornerRadius = passwordTextField.bounds.height / 2
+        enterButton.layer.cornerRadius = enterButton.bounds.height / 2
+        phoneText.layer.cornerRadius = phoneText.bounds.height / 2
+    }
 }
 
